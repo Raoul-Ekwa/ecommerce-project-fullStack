1f3d5ee023fa505d770f236550bdda9ffb94eafb
Raoul-Ekwa <tempeteekwa1995@gmail.com>
2025-02-26 17:47:42 +0100

    6th commit

--- App.js
+++ App.js
@@ -1,19 +1,29 @@
 
 import StackNavigator from './navigation/StackNavigator';
 import React from 'react';
-import { ScrollView, StyleSheet, Text, StatusBar, TouchableOpacity, View } from 'react-native';
+import { ScrollView, StyleSheet, Text, StatusBar, TouchableOpacity, View, SafeAreaView } from 'react-native';
 
 
 export default function App() {
   return (
-    <>
-      <StackNavigator />
-    </>
+      <SafeAreaView style={styles.container}>
+        <View >
+          <Text>E-Commerce App </Text>
+        </View>
+      </SafeAreaView>
+
+
+    // <>
+    //   <StackNavigator />
+    // </>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
     backgroundColor: '#faf2f9',
   },
 });
